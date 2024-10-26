18e9178fdfbdaa1f9e5871f5308b41689d2a56d0
sachinhd517 <sachinhd517@gmail.com>
2024-10-26 11:22:31 +0530

    I did add User Table in Supabase "npm run db:run"

---
+++ migrate.ts
@@ -0,0 +1,9 @@
+import { connection, db } from "@/db/db";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+
+
+
+(async () => {
+    await migrate(db, {migrationsFolder: './drizzle'});
+    await connection.end();
+})();

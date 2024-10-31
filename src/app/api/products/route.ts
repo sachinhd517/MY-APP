0e8406dcfcc1e299c438d4b23abbfeb4e7a3922c
sachinhd517 <sachinhd517@gmail.com>
2024-10-31 19:16:53 +0530

    get product endpoint

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -59,8 +59,10 @@ export async function POST(request: Request) {
 export async function GET()
 {
     try {
+
         const allProducts = await db.select().from(products).orderBy(desc(products.id));
         return Response.json(allProducts);
+
     } catch (err) {
         return Response.json({ message: 'Failed to fetch products.'}, {status: 500});
     }
